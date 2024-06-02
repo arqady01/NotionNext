a6b9a36e9a32def5a403fe4db4ae1279368567f3
arqady01 <36727217+arqady01@users.noreply.github.com>
2024-06-02 10:56:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,8 +20,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'https://space.bilibili.com/384974886',
   HEO_HERO_TITLE_4: '值得纪念',
-  HEO_HERO_TITLE_5: '先来看部影片放松一下吧',
-  HEO_HERO_TITLE_LINK: 'www.bilibili.com',
+  HEO_HERO_TITLE_5: '先看部影片放松一下吧',
+  HEO_HERO_TITLE_LINK: 'https://www.bilibili.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '知行合一', url: '/tag/知行合一' },
@@ -104,9 +104,9 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_2: '加入我的社群讨论分享',
+  HEO_SOCIAL_CARD_TITLE_3: '加入Telegram->',
+  HEO_SOCIAL_CARD_URL: 'https://t.me/+NHV7SysSVaE2ZDA1',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
